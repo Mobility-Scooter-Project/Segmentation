cd2fded3b5f2e17b8da22f4712604480c0f348b8
Melvin <melvingit@gmail.com>
2023-11-29 01:41:24 -0800

    start SAM class

--- sam.py
+++ sam.py
@@ -3,8 +3,10 @@ Segment Anything Model (SAM) File
 -A class that contains all SAM model data
 '''
 import torch
+from segment_anything import sam_model_registry, SamPredictor
 
 class SegmentAnything:
+    # Initializes model
     def __init__(self):
         # Check if CUDA is available and use GPU if it is
         if torch.cuda.is_available():
@@ -17,4 +19,7 @@ class SegmentAnything:
         sam_checkpoint = "sam_vit_b_01ec64.pth"
         model_type = "vit_b"
         
-        
+        # Define model and set it to device
+        self.model = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+        self.model.to(device=device)
+        self.predictor = SamPredictor(self.model)
