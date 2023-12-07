0578ea1fedab5d0313b12aba9c45cd0e8c3621cb
Melvin <melvingit@gmail.com>
2023-12-06 19:58:18 -0800

    refactor video file into a class

--- segment.py
+++ segment.py
@@ -1,6 +1,15 @@
 '''
-
+Segment File
+-Uses the classes in the SAM file to process videos
 '''
 
+from sam import SegmentAnything
+from videos import Video
+
 def process_file(in_file, out_file, interval, video_output):
+    # instantiate the SegmentAnything and Video classes
+    pmodel = SegmentAnything()
+    cap = Video(in_file)
+
+    
     print("process_file working!")
--- videos.py
+++ videos.py
@@ -2,40 +2,45 @@
 Videos File
 -Handles all logic relating to video manipulation
 '''
+import cv2
 
-# Retrieves the frames from the inputted video and returns a list of frames based on the given interval.
-# An alternate implementation which does not iterate through every single frame is commented out because
-# the video.set(cv2.CAP_PROP_POS_FRAMES, count - 1) function was found to be slow.
-def get_frames(video, interval):
-    # 
-    success, frame = video.read()
-    count = 1
-    frames = []
+class Video:
+    def __init__(self, video_src):
+        self.video = cv2.VideoCapture(video_src)
+    
+    # Retrieves the frames from the inputted video and returns a list of frames based on the given interval.
+    # An alternate implementation which does not iterate through every single frame is commented out because
+    # the video.set(cv2.CAP_PROP_POS_FRAMES, count - 1) function was found to be slow.
+    def get_all_frames(self, interval):
+        # 
+        success, frame = self.video.read()
+        count = 1
+        frames = []
 
-    while success:
-        if count % interval == 0:
-            frames.append(frame)
-        
-        success, frame = video.read()
-        count += 1
+        while success:
+            if count % interval == 0:
+                frames.append(frame)
+            
+            success, frame = self.video.read()
+            count += 1
 
 
-    '''
-    total_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
-    count = interval
-    video.set(cv2.CAP_PROP_POS_FRAMES, count - 1)
-    success, frame = video.read()
-
-    while count <= total_frames and success:
-        frames.append(frame)
-        print("Frame ", count, "Saved.")
-        count += interval
+        '''
+        total_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+        count = interval
         video.set(cv2.CAP_PROP_POS_FRAMES, count - 1)
         success, frame = video.read()
-    '''
-    print("Retrieved Frames.")
-    return frames
 
-# Generates a new video with the given frames and bounding box that 
-def create_video(frames, masks, box):
-    pass
+        while count <= total_frames and success:
+            frames.append(frame)
+            print("Frame ", count, "Saved.")
+            count += interval
+            video.set(cv2.CAP_PROP_POS_FRAMES, count - 1)
+            success, frame = video.read()
+        '''
+        print("Retrieved Frames.")
+        return frames
+
+    # Generates a new video with the given frames and bounding box that 
+    def create_video(frames, masks, box):
+        pass
